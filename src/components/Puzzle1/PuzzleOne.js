e33a768047250294ad727a91ddfd2aca53ab8dd2
Avery Berryman <averyberryman@gmail.com>
2023-10-15 17:39:16 -0600

    Create first puzzle component

---
+++ src/components/Puzzle1/PuzzleOne.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './PuzzleOne.css'
+
+export default function Puzzle1({ onClose, onSubmit }) {
+  const [answer, setAnswer] = useState('');
+
+  const handleSubmit = () => {
+    onSubmit(answer);
+    onClose();
+  };
+
+  return (
+    <div className="popup">
+      <h2>What's that displayed on Bob's computer? Looks like a cipher...maybe he kept some notes on his whiteboard? </h2>
+      <p>Decode the cipher: what does the message on Bob's computer mean?</p>
+      <input
+        type="text"
+        value={answer}
+        onChange={(e) => setAnswer(e.target.value)}
+        placeholder="Enter answer"
+      />
+      <button onClick={handleSubmit}>Submit</button>
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
+}
