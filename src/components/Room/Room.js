aef098f767a66c02b88460e7e5a230e53ec0688f
Avery Berryman <averyberryman@gmail.com>
2023-10-16 13:57:25 -0600

    Fix merge conflict bt puzzle two and four

--- src/components/Room/Room.js
+++ src/components/Room/Room.js
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
-import PuzzleOne from '../PuzzleOne/PuzzleOne';
-import PuzzleTwo from '../PuzzleTwo/PuzzleTwo'; 
-import './Room.css';
-import blueBackground from '../../assets/room/blue-bg.png';
-import clock from '../../assets/room/clock.png';
-import plant from '../../assets/room/plant.png';
-import door from '../../assets/room/door.png';
-import board from '../../assets/room/board.png';
-import radio from '../../assets/room/radio.png'; 
-import lamp from '../../assets/room/lamp.png';
-import deskComp from '../../assets/room/desk-comp.png';
-import bike from '../../assets/room/bike-front.png';
+import React, { useState } from "react";
+import PuzzleOne from "../PuzzleOne/PuzzleOne";
+import PuzzleTwo from "../PuzzleTwo/PuzzleTwo";
+import PuzzleFour from "../PuzzleFour/PuzzleFour";
+import "./Room.css";
+import blueBackground from "../../assets/room/blue-bg.png";
+import clock from "../../assets/room/clock.png";
+import plant from "../../assets/room/plant.png";
+import door from "../../assets/room/door.png";
+import board from "../../assets/room/board.png";
+import radio from "../../assets/room/radio.png";
+import lamp from "../../assets/room/lamp.png";
+import deskComp from "../../assets/room/desk-comp.png";
+import bike from "../../assets/room/bike-front.png";
 
 export default function Room() {
   const [showPuzzleOne, setShowPuzzleOne] = useState(false);
   const [showPuzzleTwo, setShowPuzzleTwo] = useState(false);
+  const [showPuzzleFour, setShowPuzzleFour] = useState(false);
 
   const handleDeskCompClick = () => {
     setShowPuzzleOne(true);
@@ -23,7 +25,6 @@ export default function Room() {
   const handleClosePuzzleOne = () => {
     setShowPuzzleOne(false);
   };
-
   const handleRadioClick = () => {
     setShowPuzzleTwo(true);
   };
@@ -32,24 +33,46 @@ export default function Room() {
     setShowPuzzleTwo(false);
   };
 
+  const handlePlantClick = () => {
+    setShowPuzzleFour(true);
+  };
+
+  const handleClosePuzzleFour = () => {
+    setShowPuzzleFour(false);
+  };
+
   return (
-    <article className='room' style={{ '--room-bg': `url(${blueBackground})` }}>
-      <button className='clock-btn'><img className='clock' src={clock} alt='clock' /></button>
-      <button className='plant-btn'><img className='plant' src={plant} alt='plant' /></button>
-      <button className='bike-btn'><img className='bike' src={bike} alt='bike' /></button>
-      <button className='door-btn'><img className='door' src={door} alt='door' /></button>
-      <button className='board-btn'><img className='board' src={board} alt='board' /></button>
-      <button className='desk-comp-btn' onClick={handleDeskCompClick}><img className='desk-comp' src={deskComp} alt='desk' /></button>
-      <button className='lamp-btn'><img className='lamp' src={lamp} alt='lamp' /></button>
-      <button className='radio-btn' onClick={handleRadioClick}><img className='radio' src={radio} alt='radio' /></button>
+    <article className="room" style={{ "--room-bg": `url(${blueBackground})` }}>
+      <button className="clock-btn">
+        <img className="clock" src={clock} alt="clock" />
+      </button>
+      <button className="plant-btn" onClick={handlePlantClick}>
+        <img className="plant" src={plant} alt="plant" />
+      </button>
+      <button className="bike-btn">
+        <img className="bike" src={bike} alt="bike" />
+      </button>
+      <button className="door-btn">
+        <img className="door" src={door} alt="door" />
+      </button>
+      <button className="board-btn">
+        <img className="board" src={board} alt="board" />
+      </button>
+      <button className="desk-comp-btn" onClick={handleDeskCompClick}>
+        <img className="desk-comp" src={deskComp} alt="desk" />
+      </button>
+      <button className="lamp-btn">
+        <img className="lamp" src={lamp} alt="lamp" />
+      </button>
+      <button className="radio-btn" onClick={handleRadioClick}>
+        <img className="radio" src={radio} alt="radio" />
+      </button>
 
-      {showPuzzleOne && (
-        <PuzzleOne onClose={handleClosePuzzleOne} />
-      )}
+      {showPuzzleOne && <PuzzleOne onClose={handleClosePuzzleOne} />}
 
-      {showPuzzleTwo && (
-        <PuzzleTwo onClose={handleClosePuzzleTwo} />
-      )}
+      {showPuzzleTwo && <PuzzleTwo onClose={handleClosePuzzleTwo} />}
+
+      {showPuzzleFour && <PuzzleFour onClose={handleClosePuzzleFour} />}
     </article>
   );
 }
