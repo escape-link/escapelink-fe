5945ab2a2a5a6665f84a08081adf90ea8ac5f776
Judy Ye <judyy.ye@gmail.com>
2023-10-17 19:41:45 -0700

    Add game logic to PuzzleFive with sad paths

--- src/components/PuzzleFive/PuzzleFive.js
+++ src/components/PuzzleFive/PuzzleFive.js
@@ -1,26 +1,41 @@
 import { useState } from 'react';
 import './PuzzleFive.css'
 
-export default function PuzzleFive ({ onClose, onSubmit }) {
+export default function PuzzleFive ({   
+  onClose,
+  winConditions,
+  setWinConditions
+}) {
   const [answer, setAnswer] = useState('');
+  const [incorrect, setIncorrect] = useState('');
+
+  const handleChange = (e) => {
+    setIncorrect('')
+    setAnswer(e.target.value)
+  }
 
   const handleSubmit = () => {
-    onSubmit(answer);
-    onClose();
+    if (answer === ('Will Smith').toLowerCase()) {
+      setWinConditions([...winConditions, answer]);
+      onClose();
+    } else {
+      setIncorrect('Incorrect: Please try again');
+    }
   };
 
   return (
     <div className="popup">
-    <h2>The slap heard round the world.</h2>
-    <p>This actor is famous for defeating an enemy society with Jeff Goldblum and also notably slapped the human known as Chris Rock. We suspect Bob has disappeared to where they came from.</p>
-    <input
-      type="text"
-      value={answer}
-      onChange={(e) => setAnswer(e.target.value)}
-      placeholder="Enter answer"
-    />
-    <button onClick={handleSubmit}>Submit</button>
-    <button onClick={onClose}>Close</button>
+      <p>{incorrect}</p>
+      <h2>The slap heard round the world.</h2>
+      <p>This actor is famous for defeating an enemy society with Jeff Goldblum and also notably slapped the human known as Chris Rock. We suspect Bob has disappeared to where they came from.</p>
+      <input
+        type="text"
+        value={answer}
+        onChange={handleChange}
+        placeholder="Enter answer"
+      />
+      <button onClick={handleSubmit}>Submit</button>
+      <button onClick={onClose}>Close</button>
   </div>
   )
 }
