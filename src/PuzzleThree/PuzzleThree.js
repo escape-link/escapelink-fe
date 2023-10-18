fc0ea54c229a682dcbb728118f33a90dd62fdb4c
Judy Ye <judyy.ye@gmail.com>
2023-10-17 19:10:58 -0700

    Add win logic and sad path alert to PuzzleThree

--- src/PuzzleThree/PuzzleThree.js
+++ src/PuzzleThree/PuzzleThree.js
@@ -1,30 +1,41 @@
 import { useState } from 'react';
-import './PuzzleThree.css'
+import './PuzzleThree.css';
 
-export default function PuzzleThree ({ onClose, onSubmit }) {
+export default function PuzzleThree({
+  onClose,
+  winConditions,
+  setWinConditions
+}) {
   const [answer, setAnswer] = useState('');
+  const [incorrect, setIncorrect] = useState('');
+
 
   const handleSubmit = () => {
-    onSubmit(answer);
-    onClose();
+    if (answer === 'alien') {
+      setWinConditions([...winConditions, answer]);
+      onClose();
+    } else {
+      setIncorrect('Incorrect: Please try again');
+    }
   };
 
   return (
     <div className="popup">
-    <h2>Riddle</h2>
-    <p>"I come from a place beyond the stars,
-    With shiny ships that blink near and far.
-    You've seen me in movies, books, and more,
-    Do you know what I am, or are you unsure?"
-    </p>
-    <input
-      type="text"
-      value={answer}
-      onChange={(e) => setAnswer(e.target.value)}
-      placeholder="Enter answer"
-    />
-    <button onClick={handleSubmit}>Submit</button>
-    <button onClick={onClose}>Close</button>
-  </div>
-  )
+      <p>{incorrect}</p>
+      <h2>Riddle</h2>
+      <p>
+        "I come from a place beyond the stars, With shiny ships that blink near
+        and far. You've seen me in movies, books, and more, Do you know what I
+        am, or are you unsure?"
+      </p>
+      <input
+        type="text"
+        value={answer}
+        onChange={(e) => setAnswer(e.target.value)}
+        placeholder="Enter answer"
+      />
+      <button onClick={handleSubmit}>Submit</button>
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
 }
