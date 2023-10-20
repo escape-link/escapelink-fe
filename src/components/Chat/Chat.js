7d40562509b18685ac99ddfd495de069b111ed13
Judy Ye <judyy.ye@gmail.com>
2023-10-20 14:02:50 -0700

    Move Chat action cable

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,46 +1,55 @@
 import './Chat.css';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { createConsumer } from '@rails/actioncable';
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
+import ChatWindow from '../ChatWindow/ChatWindow';
+// import { WebSocketContext } from '../Context/WebSocketContext';
 
-export default function Chat() {
+
+
+export default function Chat({subscription, allMessages}) {
+  // const subscription = useContext(WebSocketContext)
+  // const location = useLocation();
+  // const allMessages = location.state.allMessages
+  // const subscription = location.state.subscription;
   const [nickname, setNickname] = useState('');
   const [currentMessage, setCurrentMessage] = useState('');
-  const [allMessages, setAllMessages] = useState([]);
-  const [subscription, setSubscription] = useState(null);
+  // const [allMessages, setAllMessages] = useState([]);
+  // const [subscription, setSubscription] = useState(null);
   const [hasNickname, setHasNickname] = useState(false);
   const { gameName } = useParams();
+  const [chatOpen, setChatOpen] = useState(false)
 
-  useEffect(() => {
-    const nicknameExist = localStorage.getItem(`nickname_${gameName}`);
-    if (nicknameExist) {
-      setNickname(nicknameExist);
-      setHasNickname(true);
-    }
+  // useEffect(() => {
+  //   const nicknameExist = localStorage.getItem(`nickname_${gameName}`);
+  //   if (nicknameExist) {
+  //     setNickname(nicknameExist);
+  //     setHasNickname(true);
+  //   }
 
-    const cable = createConsumer(
-      // 'wss://escapelink-be-42ffc95e6cf7.herokuapp.com/cable'
-      'ws://localhost:3000/cable'
-    );
-    const newSubscription = cable.subscriptions.create(
-      { channel: 'GameChannel', game: gameName },
-      {
-        received: (data) => {
-          setAllMessages((allMessages) => [
-            ...allMessages,
-            `${data.nickname}: ${data.message}`
-          ]);
-        }
-      }
-    );
+  //   const cable = createConsumer(
+  //     // 'wss://escapelink-be-42ffc95e6cf7.herokuapp.com/cable'
+  //     'ws://localhost:3000/cable'
+  //   );
+  //   const newSubscription = cable.subscriptions.create(
+  //     { channel: 'GameChannel', game: gameName },
+  //     {
+  //       received: (data) => {
+  //         setAllMessages((allMessages) => [
+  //           ...allMessages,
+  //           `${data.nickname}: ${data.message}`
+  //         ]);
+  //       }
+  //     }
+  //   );
 
-    setSubscription(newSubscription)
+  //   setSubscription(newSubscription)
 
-    return () => {
-      cable.disconnect()
-      newSubscription.unsubscribe()
-    }
-  }, [gameName]);
+  //   return () => {
+  //     cable.disconnect()
+  //     newSubscription.unsubscribe()
+  //   }
+  // }, [gameName]);
 
   const handleSubmitMessage = (e) => {
     subscription.send({
@@ -55,8 +64,14 @@ export default function Chat() {
     setHasNickname(true);
   };
 
+  const openPopOutChat = () => {
+    setChatOpen(!chatOpen)
+  }
+  
   return (
-    <div>
+    <div className='chat'>
+       <button className='see-message-btn' onClick={openPopOutChat}>See Messages</button>
+    {chatOpen && <ChatWindow allMessages={allMessages} currentMessage={currentMessage} setCurrentMessage={setCurrentMessage} handleSubmitMessage={handleSubmitMessage} openPopOutChat={openPopOutChat} />}
       {!hasNickname ? (
         <div>
           <label htmlFor="nickname" />
