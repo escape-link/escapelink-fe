10bf9bb5edaada22066ff39c58a8230612a3f3a3
Judy Ye <judyy.ye@gmail.com>
2023-10-20 16:00:26 -0700

    Add ErrorHandling page

--- src/components/ErrorHandling/ErrorHandling.js
+++ src/components/ErrorHandling/ErrorHandling.js
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom'
 import './ErrorHandling.css'
 
 export default function ErrorHandling() {
   return (
-    <div>
-      <h2>Error</h2>
+    <div className='error-display'>
+      <h2 >Oops! nothing to see here</h2>
+      <Link className='retry' to={'/'}>Retry</Link>
     </div>
   )
 }
