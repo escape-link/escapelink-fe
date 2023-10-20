969e8ec4226e3a3b9dc0fa0fffd54ff09eb40d09
Judy Ye <judyy.ye@gmail.com>
2023-10-20 11:39:36 -0700

    Add ChaWindow component for popout chat

---
+++ src/components/ChatWindow/ChatWindow.js
@@ -0,0 +1,30 @@
+import './ChatWindow.css'
+
+export default function ChatWindow({currentMessage, setCurrentMessage, handleSubmitMessage, openPopOutChat, allMessages}) {
+
+    const messagesToAdd = allMessages.map((message, index) => (
+      <div key={index}className='popout-chat'>
+        <p >{message}</p>
+    </div> 
+    ));
+  
+    return <div className='messages-container'>
+      <button onClick={() => openPopOutChat()}>X</button>
+      {messagesToAdd}
+      <label htmlFor='currentMessage'/>
+      <textarea 
+        id='currentMessage'
+        name='currentMessage'
+        placeholder='Enter message here'
+        type='text'
+        rows='4'
+        cols='33'
+        value={currentMessage}
+        onChange={e => setCurrentMessage(e.target.value)}
+      />
+      <button onClick={handleSubmitMessage}>Send</button>
+      
+      </div>;
+  
+
+}
