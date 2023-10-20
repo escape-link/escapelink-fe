3ea8c901bfb93dd3a1274a536219c6807f5d403f
Judy Ye <judyy.ye@gmail.com>
2023-10-20 14:02:24 -0700

    Move components

--- src/App.js
+++ src/App.js
@@ -6,22 +6,25 @@ import Chat from './components/Chat/Chat';
 import LandingPage from './components/LandingPage/LandingPage';
 import RoomOne from './components/RoomOne/RoomOne';
 import RoomHeader from './components/RoomHeader/RoomHeader';
+// import { WebSocketContextProvider } from './components/Context/WebSocketContext';
 
 function App() {
   return (
-    <div>
-      <Routes>
-        {/* Single route for the landing page */}
-        <Route path='/' element={<><Header /><LandingPage /></>} />
 
-        {/* Route when only the room name is present in the URL */}
-        <Route path='/:displayedRoomName' element={<><Header /><Story /></>} />
+      <div>
+        <Routes>
+          {/* Single route for the landing page */}
+          <Route path='/' element={<><Header /><LandingPage /></>} />
+  
+          {/* Route when only the room name is present in the URL */}
+          <Route path='/:displayedRoomName' element={<><Header /><Story /></>} />
+  
+          {/* New route to capture data sent by the backend */}
+          <Route path='/:displayedRoomName/:backendData' element={<RoomOne />} />
+        </Routes>
+      </div>
 
-        {/* New route to capture data sent by the backend */}
-        <Route path='/:displayedRoomName/:backendData' element={<><RoomOne /><Chat /></>} />
-      </Routes>
-    </div>
-  );
+    );
 }
 
 export default App;
