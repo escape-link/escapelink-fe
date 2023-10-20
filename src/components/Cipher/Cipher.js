dd316d84b3f7dc8bf9554d01f03fac890ea55523
Avery Berryman <averyberryman@gmail.com>
2023-10-20 11:11:10 -0600

    created cipher component, added image and css

---
+++ src/components/Cipher/Cipher.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./Cipher.css";
+import "../../assets/room/Cipher.png";
+
+export default function Cipher({ onClose }) {
+  return (
+    <div className="popup">
+      <h2>
+        Here are some of Bob's notes. Looks like he got ahead of us on the
+        cipher.
+      </h2>
+      <p>
+        A ⏃ B ⏚ C ☊ D ⎅ E ⟒ F ⎎ G ☌ H ⊑ I ⟟ J ⟊ K ☍ L ⌰ M ⋔ N ⋏ O ⍜ P ⌿ Q ⍾ R ⍀
+        S ⌇ T ⏁ U ⎍ V ⎐ W ⍙ X ⌖ Y ⊬ Z ⋉
+      </p>
+      <p>Decode the cipher: ⌿⍜⍀⏁⏃⌰</p>
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
+}
