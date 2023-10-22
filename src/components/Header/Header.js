15c36ec5bb800d2df8a0c7b463eef985a2489145
Judy Ye <judyy.ye@gmail.com>
2023-10-21 18:35:15 -0700

    Remove code to pass CI

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,8 @@
 import './Header.css'
-import alien from '../../assets/alien.png'
+// import alien from '../../assets/alien.png'
 // import logo from '../../../public/escapelinklogo.png'
 
 export default function Header() {
-  const logo = '/escapelinklogoV2.png'
   return (
     <header>
       <h1>Escape Link</h1>
