0276217eb4696d50639ad1aa6dfcf046fb2173bc
Judy Ye <judyy.ye@gmail.com>
2023-11-29 11:04:58 -0800

    Add route to Leaderboard

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
 import RoomOne from './components/RoomOne/RoomOne';
 import ErrorHandling from './components/ErrorHandling/ErrorHandling';
+import Leaderboard from './components/Leaderboard/Leaderboard';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path='/' element={<><Header /><LandingPage /></>} />
           <Route path='room/:displayedRoomName' element={<><Header /><Story /></>} />
           <Route path='roomOne/:displayedRoomName/:backendData' element={<RoomOne />} />
+          <Route path='leaderboard' element={<Leaderboard />}/>
           <Route path='*' element={<ErrorHandling />}/>
         </Routes>
       </div>
