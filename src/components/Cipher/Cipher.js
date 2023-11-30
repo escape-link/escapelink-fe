2a2a09a54fe14d2690c69d17a7f1b65101999fe1
Judy Ye <judyy.ye@gmail.com>
2023-11-30 10:31:41 -0800

    Add tab index and useRef hook to Cipher

--- src/components/Cipher/Cipher.js
+++ src/components/Cipher/Cipher.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import "./Cipher.css";
 
 export default function Cipher({ onClose }) {
+  const modalRef = useRef()
+
+  useEffect(() => {
+    if (modalRef.current) {
+      modalRef.current.focus()
+    }
+  }, [])
+
   return (
-    <div className="popup">
-      <h2>
+    <div tabIndex="-1" ref={modalRef} className="popup">
+      <h1 tabIndex='0'>
         Here are some of Bob's notes. Looks like he got ahead of us on the
         cipher.
-      </h2>
-      <p>
+      </h1>
+      <p tabIndex='0'>
         A ⏃ B ⏚ C ☊ D ⎅ E ⟒ F ⎎ G ☌ H ⊑ I ⟟ J ⟊ K ☍ L ⌰ M ⋔ N ⋏ O ⍜ P ⌿ Q ⍾ R ⍀
         S ⌇ T ⏁ U ⎍ V ⎐ W ⍙ X ⌖ Y ⊬ Z ⋉
       </p>
