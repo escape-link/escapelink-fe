bcf291ca5d7d5c07994679b4c4664b10c0119223
Judy Ye <judyy.ye@gmail.com>
2023-12-01 09:06:09 -0800

    Add a copy link to Story and move tab index to a child

--- src/components/Story/Story.js
+++ src/components/Story/Story.js
@@ -11,11 +11,45 @@ export default function Story() {
   const { displayedRoomName } = useParams();
   const [openLeaderboard, setOpenLeaderboard] = useState(false);
   const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const decodedDisplayedRoomName = encodeURIComponent(displayedRoomName);
+  const [copied, setCopied] = useState(false);
+  const [gameName, setGameName] = useState(null);
+  const [gameData, setGameData] = useState(null);
 
-  const startGame = async () => {
+  function copy() {
+    const additionalValue = 'One';
+    const updatedUrl = `${window.location.origin}/room${additionalValue}/${decodedDisplayedRoomName}/${gameName}`;
+
+    const el = document.createElement('input');
+    el.value = updatedUrl;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  }
+
+  useEffect(() => {
+    const getRoomName = async () => {
+      try {
+        const decodedDisplayedRoomName = encodeURIComponent(displayedRoomName);
+        const data = await fetchGameLink(decodedDisplayedRoomName);
+        setGameData(data);
+        setGameName(data.game_name);
+      } catch (err) {
+        console.log(`${err.message}`);
+      }
+    };
+    getRoomName();
+  }, [displayedRoomName]);
+
+  const startGame = () => {
     try {
       const decodedDisplayedRoomName = encodeURIComponent(displayedRoomName);
-      const gameData = await fetchGameLink(decodedDisplayedRoomName);
       navigate(`/roomOne/${decodedDisplayedRoomName}/${gameData.game_name}`);
     } catch (err) {
       console.log(`${err.message}`);
@@ -38,59 +72,68 @@ export default function Story() {
     <section
       className="story-body"
       style={{ '--background': `url(${background})` }}>
-         {!isImageLoaded ? <Loading /> : 
-         <>
-      {openLeaderboard && (
-        <Leaderboard handleLeaderboardClick={handleLeaderboardClick} />
+      {!isImageLoaded ? (
+        <Loading />
+      ) : (
+        <>
+          {openLeaderboard && (
+            <Leaderboard handleLeaderboardClick={handleLeaderboardClick} />
+          )}
+          <div className="back-home-container">
+            <Link to={'/'} className="back-home">
+              Back To Home
+            </Link>
+          </div>
+
+          <div className="story-and-button">
+            <div tabIndex="0" className="story">
+              <p>
+                You and your team are fringe scientists working on top-secret
+                experimental airframes in a Nevada black site for the US Air
+                Force. Bob, your eccentric colleague, has gone missing, and
+                strange occurrences have been happening in his office.
+              </p>
+              <br></br>
+              <p>
+                Bob is a peculiar scientist known for his eccentricity. He's
+                been missing for a few days, and unusual sounds and radio
+                broadcasts from his office have raised concerns. Your base
+                commander initiated a lockdown and access override after Bob
+                mysteriously disappeared on Doughnut Friday, a tradition he
+                never misses.
+              </p>
+              <br></br>
+              <p>
+                Objective: Find clues in Bob's office to uncover his whereabouts
+                and the secrets he may have left behind.
+              </p>
+              <br></br>
+              <p>
+                Now it's your team's mission to enter Bob's office to find out
+                what happened.
+              </p>
+              <p>Are you ready to begin your escape room adventure?</p>
+            </div>
+            <button className="start-game" onClick={startGame}>
+              Start Game
+            </button>
+
+            <button className="copy" onClick={copy}>
+              {!copied ? 'Copy link to Share' : 'Copied!'}
+            </button>
+          </div>
+
+          <div className="see-leaderboard-container">
+            {!openLeaderboard && (
+              <button
+                className="see-leaderboard"
+                onClick={handleLeaderboardClick}>
+                View Leaderboard
+              </button>
+            )}
+          </div>
+        </>
       )}
-      <div className="back-home-container">
-        <Link to={'/'} className="back-home">
-          Back To Home
-        </Link>
-      </div>
-
-      <div tabIndex="0" className="story-and-button">
-        <div className="story">
-          <p>
-            You and your team are fringe scientists working on top-secret
-            experimental airframes in a Nevada black site for the US Air Force.
-            Bob, your eccentric colleague, has gone missing, and strange
-            occurrences have been happening in his office.
-          </p>
-          <br></br>
-          <p>
-            Bob is a peculiar scientist known for his eccentricity. He's been
-            missing for a few days, and unusual sounds and radio broadcasts from
-            his office have raised concerns. Your base commander initiated a
-            lockdown and access override after Bob mysteriously disappeared on
-            Doughnut Friday, a tradition he never misses.
-          </p>
-          <br></br>
-          <p>
-            Objective: Find clues in Bob's office to uncover his whereabouts and
-            the secrets he may have left behind.
-          </p>
-          <br></br>
-          <p>
-            Now it's your team's mission to enter Bob's office to find out what
-            happened.
-          </p>
-          <p>Are you ready to begin your escape room adventure?</p>
-        </div>
-        <button className="start-game" onClick={startGame}>
-          Start Game
-        </button>
-      </div>
-
-      <div className="see-leaderboard-container">
-        {!openLeaderboard && (
-          <button className="see-leaderboard" onClick={handleLeaderboardClick}>
-            View Leaderboard
-          </button>
-        )}
-      </div>
-      </>
-}
     </section>
   );
 }
